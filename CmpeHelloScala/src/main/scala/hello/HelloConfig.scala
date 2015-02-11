145e57da3ef89f9b6851304ade8a704dac7fcf47
gargsurbhi <garg.surbhi90@gmail.com>
2015-02-10 17:48:31 -0800

    code push

---
+++ CmpeHelloScala/src/main/scala/hello/HelloConfig.scala
@@ -0,0 +1,31 @@
+package hello
+
+
+
+
+import org.springframework.context.annotation.{Configuration, ComponentScan}
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot;
+import org.springframework.web.bind.annotation.RequestMapping; 
+import org.springframework.web.bind.annotation.ResponseBody; 
+import org.springframework.stereotype.Controller; 
+
+/**
+ * This config class will trigger Spring @annotation scanning and auto configure Spring context.
+ *
+ * @author surbhi
+ * @since 1.0
+ */
+
+@EnableAutoConfiguration
+@ComponentScan
+@Controller
+@Configuration
+class HelloConfig {
+
+@RequestMapping(Array("/"))
+@ResponseBody
+def home(): String = "Hello World!"
+
+
+}
